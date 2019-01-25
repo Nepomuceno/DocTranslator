be8bb62d9ee988e6b0e7b0d10008c5a940503eb3
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2019-01-25 11:01:07 +0000

    adding documents

---
+++ src/api/Controllers/DocsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace DocTranslatorApi.Controllers
+{
+    [Route("api/docs")]
+    [ApiController]
+    public class DocsController : ControllerBase
+    {
+        private WordTranslator _translator;
+        public DocsController(WordTranslator translator)
+        {
+            _translator = translator;
+        }
+        [HttpPost("upload")]
+        public async Task<string> Upload(List<IFormFile> files)
+        {
+            long size = files.Sum(f => f.Length);
+            Console.WriteLine("Bla");
+            // full path to file in temp location
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    if (formFile.FileName.EndsWith(".docx"))
+                    {
+                        var filePath = await _translator.Translate(formFile);
+                        //await UploadFileToStorage(stream, $"{formFile.FileName}");
+                        using (FileStream streamfile = System.IO.File.Open(filePath, FileMode.Open))
+                        {
+                            await UploadFileToStorage(streamfile, $"{DateTime.UtcNow.Ticks}.docx");
+                        }
+
+                    }
+                }
+            }
+            return "OK";
+        }
+        private static Task UploadFileToStorage(Stream fileStream, string fileName)
+        {
+            StorageCredentials storageCredentials = new StorageCredentials("ACCOUNT-NAME", "STORAGE-ACCOUNT-KEY");
+            CloudStorageAccount storageAccount = new CloudStorageAccount(storageCredentials, true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+            CloudBlobContainer container = blobClient.GetContainerReference("docs");
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            return blockBlob.UploadFromStreamAsync(fileStream);
+        }
+    }
+
+}
+
---
+++ src/api/Library/WordTranslator.cs
@@ -0,0 +1,92 @@
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using Microsoft.AspNetCore.Http;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+public class WordTranslator
+{
+    private HttpClient _client;
+    public WordTranslator()
+    {
+        _client = new HttpClient();
+    }
+    const string host = "https://api.cognitive.microsofttranslator.com";
+    const string route = "/translate?api-version=3.0&to=en";
+    const string subscriptionKey = "COGNITIVE-KEY";
+    public async Task<string> Translate(IFormFile file)
+    {
+        var filePath = Path.GetTempFileName();
+        Console.WriteLine($"[FILE] {filePath}");
+        using (var ms = new MemoryStream())
+        {
+            await file.CopyToAsync(ms);
+            using (WordprocessingDocument worddoc = WordprocessingDocument.Open(ms, true))
+            {
+                OpenXmlPowerTools.SimplifyMarkupSettings settings = new OpenXmlPowerTools.SimplifyMarkupSettings
+                {
+                    AcceptRevisions = false,
+                    NormalizeXml = false,         //setting this to false reduces translation quality, but if true some documents have XML format errors when opening
+                    RemoveBookmarks = true,
+                    RemoveComments = true,
+                    RemoveContentControls = true,
+                    RemoveEndAndFootNotes = true,
+                    RemoveFieldCodes = true,
+                    RemoveGoBackBookmark = true,
+                    RemoveHyperlinks = false,
+                    RemoveLastRenderedPageBreak = true,
+                    RemoveMarkupForDocumentComparison = true,
+                    RemovePermissions = false,
+                    RemoveProof = true,
+                    RemoveRsidInfo = true,
+                    RemoveSmartTags = true,
+                    RemoveSoftHyphens = true,
+                    RemoveWebHidden = true,
+                    ReplaceTabsWithSpaces = false
+                };
+                OpenXmlPowerTools.MarkupSimplifier.SimplifyMarkup(worddoc, settings);
+                Body body = worddoc.MainDocumentPart.Document.Body;
+                var texts = body.Descendants<Text>();
+                foreach (var text in texts)
+                {
+                    if (!string.IsNullOrWhiteSpace(text.Text))
+                        text.Text = await this.CallTranslator(text.Text);
+                }
+                var clone = worddoc.Clone(filePath);
+                clone.Close();
+                worddoc.Close();
+            }
+        }
+        return filePath;
+    }
+
+    private async Task<string> CallTranslator(string sentence)
+    {
+        System.Object[] body = new System.Object[] { new { Text = sentence } };
+        var requestBody = JsonConvert.SerializeObject(body);
+        using (var request = new HttpRequestMessage())
+        {
+            // Set the method to POST
+            request.Method = HttpMethod.Post;
+            // Construct the full URI
+            request.RequestUri = new Uri(host + route);
+            // Add the serialized JSON object to your request
+            request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+            // Add the authorization header
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            // Send request, get response
+            var response = await _client.SendAsync(request);
+            var jsonResponse = await response.Content.ReadAsStringAsync();
+            dynamic content = JsonConvert.DeserializeObject(jsonResponse);
+            // Print the response
+            return content[0].translations[0].text;
+        }
+    }
+}
